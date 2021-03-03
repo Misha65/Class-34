30df4468a37b5ede8df5e3ab68405a926d3ab2e0
Misha65 <73336746+Misha65@users.noreply.github.com>
2021-03-03 15:04:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,64 @@
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+const Engine= Matter.Engine;
+const World= Matter.World;
+const Bodies= Matter.Bodies;
+const Constraint=Matter.Constraint;
+
+var engine;
+var world;
+
+
+function setup(){
+createCanvas(1000,800);
+engine=Engine.create();
+world=engine.world;
+ground=new Ground(500,800,1000,20);
+box1=new Box(700,500,70,70);
+box2=new Box(700,500,70,70);
+box3=new Box(700,500,70,70);
+box4=new Box(700,500,70,70);
+box5=new Box(700,500,70,70);
+
+box6=new Box(900,500,70,70);
+box7=new Box(900,500,70,70);
+box8=new Box(900,500,70,70);
+box9=new Box(900,500,70,70);
+box10=new Box(900,500,70,70);
+
+box11=new Box(800,500,70,70);
+box12=new Box(800,500,70,70);
+box13=new Box(800,500,70,70);
+box14=new Box(800,500,70,70);
+box15=new Box(800,500,70,70);
+
+
+ball=new Ball(500,400,90,90)
+
+rope=new Rope(ball.body,{x:200,y:300})
 }
 
-function draw() {
-  background(255,255,255);  
-  drawSprites();
+function draw(){
+  background(0);
+  Engine.update(engine);
+  ground.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+  box7.display();
+  box8.display();
+  box9.display();
+  box10.display();
+  box11.display();
+  box12.display();
+  box13.display();
+  box14.display();
+  box15.display();
+  ball.display();
+  rope.display();
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(ball.body,{x:mouseX,y:mouseY})
 }
